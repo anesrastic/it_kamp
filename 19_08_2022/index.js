abfa066bc1d6e219f1f9c389a60198e75bd15f1c
anesrastic <anesrastic@gmail.com>
2022-08-20 18:34:17 +0200

    domci_JS_19_08_2022

--- 19_08_2022/index.js
+++ 19_08_2022/index.js
@@ -1,10 +1,9 @@
-function ispisiOblik(n) {
-    for (let i = 1; i <= n; i++) {
-      var ispis = "";
-      console.log("\n")
-       for (let j = 1; j <= i; j++) {
-         ispis+=j;
-          console.log(ispis);
+function ispisiOblik(n){
+  for(let i=1; i<=n; i++){
+    for(let j=1; j<=i; j++){
+      document.write(j);
       }
-    }
+      document.write("<br>");
+  }
 }
+ispisiOblik(8);
